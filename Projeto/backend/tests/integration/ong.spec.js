ecbf7202ed4853899fd4541d7fc6cd30dc150139
JuniorN1 <junior_e.a.o@hotmail.com>
2021-08-15 22:10:35 -0300

    fix bugs and update project  npm to yarn

--- Projeto/backend/tests/integration/ong.spec.js
+++ Projeto/backend/tests/integration/ong.spec.js
@@ -1,21 +1,19 @@
-//import { isIterable } from "core-js";
 const  request = require('supertest');
 const  app = require('../../src/app');
 const  conection = require('../../src/database/connection');
 describe('ONG',()=>{
-    beforeEach(async()=>{
-        await conection.migrate.rollback();
-        await conection.migrate.latest();
+    beforeEach(async()=>{    
+        await conection.migrate.rollback;
+        await conection.migrate.latest;   
 
     });
-    afterAll(async()=>{
+    afterAll(async()=>{  
        await conection.destroy();
-
     });
     it('Should be able to create a new ONG',async()=>{
+        
         const response = await request(app)
         .post('/ongs')
-        //.set('authorization','asas')
         .send({            
                 name:"junior",
                 email:"contato@mail.com",
--- Projeto/backend/tests/unit/gerenateUniqueId.spec.js
+++ Projeto/backend/tests/unit/gerenateUniqueId.spec.js
@@ -1,4 +1,3 @@
-//import { isIterable } from "core-js";
 const  generateUniqueId = require('../../src/utils/gerenateUniqueId');
 describe('Generate Unique ID',()=>{
     it('should generate an unique ID',()=>{
